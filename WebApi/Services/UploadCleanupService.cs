0fd453dcc1e5183cd0cf70ef654158178850cb5b
Mohammad <nazari.mohammad80@icloud.com>
2026-08-14 14:21:18 +0330

    chore(P0): remove obsolete UploadCleanupService stub (R3)

--- WebApi/Services/UploadCleanupService.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApi.Services;
-
-// Obsolete: cleanup is owned solely by WebApi.BackgroundServices.OrphanCleanupService.
-// This type is kept only so old project references do not break; it is not registered in DI.
-[Obsolete("Use OrphanCleanupService only.")]
-public sealed class UploadCleanupService
-{
-    private UploadCleanupService() { }
-}
